f160e058694507054986e70d09589ab0534a48e8
Tetsuya Morizumi <tetsuya.morizumi@gmail.com>
2015-10-20 22:25:16 +0900

    Clean up
    Change space to tab.
    Change comments.

--- vinst.pl
+++ vinst.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 #-----------------------------------------------------------------------------#
-# vinst.pl
-# generate instance from verilog rtl.
+#  vinst.pl
+#  generate instance snippet from verilog rtl.
 #
-# author: Tetsuya Morizumi
-# created: 2013-05-29
-# $Id$
+#  Copyright (C) Tetsuya Morizumi
+# 
+#  created: 2013-05-29
 #-----------------------------------------------------------------------------#
 use strict;
 use warnings;
@@ -16,8 +16,8 @@ use Verilog::Netlist;
 # global environment
 #-----------------------------------------------------------------------------#
 my $indent = "    ";
-my $ts = 4;             # tabstop
-my $inst_px = "u_";     # instance prefix
+my $ts = 4;			# tabstop
+my $inst_px = "u_";	# instance prefix
 #my $inst_px = "";
 
 #-----------------------------------------------------------------------------#
@@ -28,16 +28,16 @@ getopts("hie", \%opt);
 
 if($opt{h} or $#ARGV == -1)
 {
-    &help();
-    exit;
+	&help();
+	exit;
 }
 
 if($opt{i})
 {
-    &make_io_info();
+	&make_io_info();
 } else
 {
-    &make_instance();
+	&make_instance();
 }
 
 exit;
@@ -45,135 +45,136 @@ exit;
 #-----------------------------------------------------------------------------#
 sub help()
 {
-    print << "__EOL__";
-Usage: ${0} [OPTION] <verilog file>...
+	print << "__EOL__";
+Usage: ${0} [OPTION] <verilog files...>
 Options
-    -h  help
-    -i  print i/o infomation
-    -e  empty port connection
+	-h  help
+	-i  print i/o infomation
+	-e  empty port connection
 __EOL__
-    exit;
+	exit;
 }
 
 sub make_instance()
 {
-    my @files = @ARGV;
-    my $netlist = new Verilog::Netlist;
+	my @files = @ARGV;
+	my $netlist = new Verilog::Netlist;
 
-    &read_verilog_files($netlist, \@files);
+	&read_verilog_files($netlist, \@files);
 
-    &print_instance($netlist);
+	&print_instance($netlist);
 }
 
 sub make_io_info()
 {
-    my @files = @ARGV;
-    my $netlist = new Verilog::Netlist;
+	my @files = @ARGV;
+	my $netlist = new Verilog::Netlist;
 
-    &read_verilog_files($netlist, \@files);
+	&read_verilog_files($netlist, \@files);
 
-    &print_io_info($netlist);
+	&print_io_info($netlist);
 }
 
 sub read_verilog_files()
 {
-    my ($netlist, $filelist) = @_;
+	my ($netlist, $filelist) = @_;
 
-    foreach my $file (@{$filelist})
-    {
-        $netlist->read_file(filename => $file);
-    }
+	foreach my $file (@{$filelist})
+	{
+		$netlist->read_file(filename => $file);
+	}
 
-    # checkfile
-    my @existfiles = $netlist->files;
-    if($#existfiles == -1)
-    {
-        &help();
-    }
-    $netlist->link();   # connection resolve
+	# checkfile
+	my @existfiles = $netlist->files;
+	if($#existfiles == -1)
+	{
+		&help();
+	}
+	$netlist->link();	# connection resolve
 }
 
 sub get_max_portlen()
 {
-    my ($ports) = @_;
-    my $max = 0;
+	my ($ports) = @_;
+	my $max = 0;
 
-    foreach my $pp (@{$ports})
-    {
-        my $size = length($pp->name);
-        if ($max < $size) { $max = $size; }
-    }
+	foreach my $pp (@{$ports})
+	{
+		my $size = length($pp->name);
+		if ($max < $size) { $max = $size; }
+	}
 
-    return $max;
+	return $max;
 }
 
 sub print_instance()
 {
-    my ($netlist) = @_;
+	my ($netlist) = @_;
 
-    foreach my $module ($netlist->modules_sorted)
-    {
-        my @ports = $module->ports_ordered;
-        if ($#ports == -1) { next; }
+	foreach my $module ($netlist->modules_sorted)
+	{
+		my @ports = $module->ports_ordered;
+		if ($#ports == -1) { next; }
 
-        print $module->name, " ", $inst_px, $module->name, "\n(\n";
+		print $module->name, " ", $inst_px, $module->name, "\n(\n";
 
-        my $max = &get_max_portlen(\@ports);
-        $max = (int($max/$ts) + 1) * $ts;
+		my $max = &get_max_portlen(\@ports);
+		$max = (int($max/$ts) + 1) * $ts;
 
-        my $format = "";
+		my $format = "";
 
-        if($opt{e})
-        {
-            $format = sprintf "$indent.%%-%ds()", $max-1;
-        } else
-        {
-            $format = sprintf "$indent.%%-%ds(%%-%ds)", $max-1, $max-1;
-        }
+		if($opt{e})
+		{
+			$format = sprintf "$indent.%%-%ds()", $max-1;
+		} else
+		{
+			$format = sprintf "$indent.%%-%ds(%%-%ds)", $max-1, $max-1;
+		}
 
-        for(my $ii = 0; $ii <= $#ports; $ii++)
-        {
-            printf $format, $ports[$ii]->name, $ports[$ii]->name;
-            if($ii != $#ports)
-            {
-                print ",\n";
-            } else
-            {
-                print "\n";
-            }
-        }
-        print ");\n\n";
-    }
+		for(my $ii = 0; $ii <= $#ports; $ii++)
+		{
+			printf $format, $ports[$ii]->name, $ports[$ii]->name;
+			if($ii != $#ports)
+			{
+				print ",\n";
+			} else
+			{
+				print "\n";
+			}
+		}
+		print ");\n\n";
+	}
 }
 
 sub print_io_info
 {
-    my ($netlist) = @_;
+	my ($netlist) = @_;
 
-    foreach my $module ($netlist->modules_sorted)
-    {
-        my @ports = $module->ports_ordered;
-        if($#ports == -1) { next; }
+	foreach my $module ($netlist->modules_sorted)
+	{
+		my @ports = $module->ports_ordered;
+		if($#ports == -1) { next; }
 
-        print "// ", $module->name, "\n";
+		print "// ", $module->name, "\n";
 
-        my $max = &get_max_portlen(\@ports);
-        $max = (int($max/$ts) + 1) * $ts;
+		my $max = &get_max_portlen(\@ports);
+		$max = (int($max/$ts) + 1) * $ts;
 
-        my $format = sprintf "%%-%ds", $max-1;
+		my $format = sprintf "%%-%ds", $max-1;
 
-        foreach my $pp (@ports)
-        {
-            print $pp->direction , "\t";
-            if(defined($pp->net->width) && $pp->net->width != 1)
-            {
-                print '[',$pp->net->msb, ':',$pp->net->lsb,']';
-            }
-            print "\t\t";
-            print $pp->name;
-            print "\n";
-        }
-        print "\n";
-    }
+		foreach my $pp (@ports)
+		{
+			print $pp->direction , "\t";
+			if(defined($pp->net->width) && $pp->net->width != 1)
+			{
+				print '[',$pp->net->msb, ':',$pp->net->lsb,']';
+			}
+			print "\t\t";
+			print $pp->name;
+			print "\n";
+		}
+		print "\n";
+	}
 }
 
+# vim: set ts=4 sw=4:
